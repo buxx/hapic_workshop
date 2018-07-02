40c9a6ab9ee15d0d20c7386524e2e4a26f4206d4
Bastien Sevajol <sevajol.bastien@gmail.com>
2018-07-02 21:10:15 +0200

    add roads

--- run.py
+++ run.py
@@ -5,7 +5,10 @@ import marshmallow
 import vlc
 import hapic
 from flask import Flask
-from flask import request
+
+from os import listdir
+from os.path import isfile
+from os.path import join
 from hapic.ext.flask import FlaskContext
 
 # Flask app
@@ -17,11 +20,24 @@ class EmptyResponseSchema(marshmallow.Schema):
     pass
 
 
-class PlayFilePathSchema(marshmallow.Schema):
-    file = marshmallow.fields.String(
+class SendInputPathSchema(marshmallow.Schema):
+    file_name = marshmallow.fields.String(required=True)
+
+
+class SendInputFilesSchema(marshmallow.Schema):
+    file = marshmallow.fields.Raw(required=True)
+
+
+class FileInfoSchema(marshmallow.Schema):
+    name = marshmallow.fields.String(
         required=True,
-        description='file path to play',
-        example='bird.avi',
+    )
+    size = marshmallow.fields.String(
+        required=False,
+    )
+    is_link = marshmallow.fields.Boolean(
+        required=False,
+        allow_none=True,
     )
 
 
@@ -42,19 +58,84 @@ class FileQuerySchema(marshmallow.Schema):
     )
 
 
-class FileOutputSchema(marshmallow.Schema):
-    name = marshmallow.fields.String(
+class PlayFilePathSchema(marshmallow.Schema):
+    file = marshmallow.fields.String(
         required=True,
+        description='file path to play',
+        example='bird.avi',
     )
-    size = marshmallow.fields.String(
-        required=False,
-    )
-    is_link = marshmallow.fields.Boolean(
-        required=False,
-    )
+
+
+# Models
+class FileInfo(object):
+    def __init__(
+        self,
+        name,
+        size,
+        is_link=None,
+    ):
+        self.name = name
+        self.size = size
+        self.is_link = is_link
 
 
 # Routes
+@hapic.with_api_doc()
+@app.route('/send/<file_name>', methods=['POST'])
+@hapic.input_path(SendInputPathSchema())
+@hapic.input_files(SendInputFilesSchema())
+@hapic.output_body(EmptyResponseSchema(), default_http_code=204)
+def send(file_name, hapic_data):
+    with open(file_name, 'wb+') as file:
+        file.write(hapic_data.files['file'].read())
+
+    return '', 204
+
+
+@hapic.with_api_doc()
+@app.route('/files')
+@hapic.input_query(FileQuerySchema())
+@hapic.output_body(FileInfoSchema(many=True))
+def files(hapic_data):
+    file_models = []
+
+    file_paths = [
+        f for f in listdir('.')
+        if isfile(join('.', f)) and f.endswith('.avi')
+    ]
+
+    for file_path in file_paths:
+        file_model = FileInfo(
+            name=file_path,
+            size=os.path.getsize(file_path),
+        )
+
+        if hapic_data.query.get('verbose'):
+            file_model.is_link = os.path.islink(file_path)
+
+        file_models.append(file_model)
+
+    return file_models
+
+
+@hapic.with_api_doc()
+@app.route('/file/<file>')
+@hapic.handle_exception(FileNotFoundError, http_code=400)
+@hapic.input_path(FilePathSchema())
+@hapic.input_query(FileQuerySchema())
+@hapic.output_body(FileInfoSchema())
+def file(file, hapic_data):
+    infos = {
+        'name': file,
+        'size': os.path.getsize(file),
+    }
+
+    if hapic_data.query.get('verbose'):
+        infos['is_link'] = os.path.islink(file)
+
+    return infos
+
+
 @hapic.with_api_doc()
 @app.route('/bird')
 @hapic.output_body(EmptyResponseSchema(), default_http_code=204)
@@ -73,24 +154,6 @@ def play_file(file, hapic_data):
     player = vlc.MediaPlayer(file_name)
     player.play()
     return '', 204
-
-
-@hapic.with_api_doc()
-@app.route('/file/<file>')
-@hapic.handle_exception(FileNotFoundError, http_code=400)
-@hapic.input_path(FilePathSchema())
-@hapic.input_query(FileQuerySchema())
-@hapic.output_body(FileOutputSchema())
-def file(file, hapic_data):
-    infos = {
-        'name': file,
-        'size': os.path.getsize(file),
-    }
-
-    if hapic_data.query.get('verbose'):
-        infos['is_link'] = os.path.islink(file)
-
-    return infos
 
 
 # doc view
